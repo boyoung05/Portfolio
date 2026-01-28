056447206608569446ce1da1c2985bbff065cb22
boyoung05 <boyoung0512@naver.com>
2026-01-28 15:39:28 +0900

    Activity & Career

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Info from "./Info";
 import Tech from "./Tech";
+import Activity  from "./Activity";
+import Award from "./Award";
 
 const About = () => {
     return(
         <div>
             <Info />
             <Tech />
+            <Activity />
+            <Award />
         </div>
     )
 }
---
+++ src/pages/About/Activity.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import "../../styles/About.css";
+
+const Activity = () => {
+    return(
+        <div className="Activity-container">
+            <div className="Activity-title">Activity & Career</div>
+                <div className="Activity-entire-wrapper">
+                    {/*크라우드 펀딩*/}
+                    <div className="Activity-wrapper">
+                        <div className="Activity-line-one">
+                            <div className="Activity-Wrapper-title">
+                                [SEED CROWD FUNDING]
+                            </div>
+                            <div className="Activity-date">
+                                [2024.04 - 2024.07]
+                            </div>
+                        </div>
+                        <div className="Activity-Wrapper-Role">
+                            [DESIGN]
+                        </div>
+                        <div className="Activity-Wrapper-content">
+                            크라우드 펀딩
+                        </div>
+                    </div>
+                    {/*SEED 동아리 임원진*/}
+                    <div className="Activity-wrapper">
+                        <div className="Activity-line-one">
+                            <div className="Activity-Wrapper-title">
+                                [SEED]
+                            </div>
+                            <div className="Activity-date">
+                                [2024.09 - 2024.12]
+                            </div>
+                        </div>
+                        <div className="Activity-Wrapper-Role">
+                            [홍보부 부원]
+                        </div>
+                        <div className="Activity-Wrapper-content">
+                            SEED 동아리 임원진 활동
+                        </div>
+                    </div>
+                    {/*2024 ICT 창업 메이커톤*/}
+                    <div className="Activity-wrapper">
+                        <div className="Activity-line-one">
+                            <div className="Activity-Wrapper-title">
+                                [2024 ICT 창업 메이커톤]
+                            </div>
+                            <div className="Activity-date">
+                                [2024. - 2024.]
+                            </div>
+                        </div>
+                        <div className="Activity-Wrapper-Role">
+                            [DESIGN]
+                        </div>
+                        <div className="Activity-Wrapper-content">
+                            ICT 창업 메이커톤
+                        </div>
+                    </div>
+                    {/*LIKE LION*/}
+                    <div className="Activity-wrapper">
+                        <div className="Activity-line-one">
+                            <div className="Activity-Wrapper-title">
+                                [LIKE LION]
+                            </div>
+                            <div className="Activity-date">
+                                [2025.03 - 2025.12]
+                            </div>
+                        </div>
+                        <div className="Activity-Wrapper-Role">
+                            [DESIGN/FRONTEND]
+                        </div>
+                        <div className="Activity-Wrapper-content">
+                            교내 창업 동아리 멋쟁이사자처럼 13기 부원 활동
+                        </div>
+                    </div>
+                    {/*2025 ICT 창업 메이커톤*/}
+                    <div className="Activity-wrapper">
+                        <div className="Activity-line-one">
+                            <div className="Activity-Wrapper-title">
+                                [2025 ICT 창업 메이커톤]
+                            </div>
+                            <div className="Activity-date">
+                                [2025. - 2025.]
+                            </div>
+                        </div>
+                        <div className="Activity-Wrapper-Role">
+                            [DESIGN]
+                        </div>
+                        <div className="Activity-Wrapper-content">
+                            ICT 창업 메이커톤
+                        </div>
+                    </div>
+                    {/*해커톤*/}
+                    <div className="Activity-wrapper">
+                        <div className="Activity-line-one">
+                            <div className="Activity-Wrapper-title">
+                                [LIKE LION 해커톤]
+                            </div>
+                            <div className="Activity-date">
+                                [2025.04 - 2025.08]
+                            </div>
+                        </div>
+                        <div className="Activity-Wrapper-Role">
+                            [DESIGN/FRONTEND]
+                        </div>
+                        <div className="Activity-Wrapper-content">
+                            멋쟁이사자처럼 전국 해커톤 참여
+                        </div>
+                    </div>
+                    {/*개인 프로젝트*/}
+                    <div className="Activity-wrapper">
+                        <div className="Activity-line-one">
+                            <div className="Activity-Wrapper-title">
+                                [SAJANG PROJECT]
+                            </div>
+                            <div className="Activity-date">
+                                [2025.03 - 2025.12]
+                            </div>
+                        </div>
+                        <div className="Activity-Wrapper-Role">
+                            [DESIGN/FRONTEND/BACKEND]
+                        </div>
+                        <div className="Activity-Wrapper-content">
+                            나만의 쇼핑몰 웹사이트 제작
+                        </div>
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default Activity;
