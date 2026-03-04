8340e463c836fcb7b9f2a44158146925d4e3deb7
boyoung05 <boyoung0512@naver.com>
2026-03-05 01:36:05 +0900

    Fix QuickMenu import casing

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Banner from "./Banner";
-import QuickMenu from "./QuickMenu";
+import QuickMenu from "./quickMenu";
 import MainActivity from "./mainActivity";
 
 const Home = () => {
